46bceffcab7c33a21b08a533e7e619fee6fc3fb1
KhalidBatran <khalood.159@live.com>
2024-09-22 01:42:10 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,9 @@
+import dash
+import dash_bootstrap_components as dbc
+from dash import Input, Output, dcc, html
+import pandas as pd
+import plotly.express as px
 
+server = app.server
+
+df = pd.read_csv("https://raw.githubusercontent.com/KhalidBatran/MCM-Final-Project/main/assets/Olympics%202024.csv")
